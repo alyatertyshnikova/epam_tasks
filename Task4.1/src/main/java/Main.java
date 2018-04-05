77e59cdb00830bbac42fee5e4c93ba26283d7181
Alina <macintosh@MacBook-Pro-Macintosh.local>
2018-04-05 17:39:28 +0300

    Task4.1-2 is Modified

---
+++ Task4.1/src/main/java/Main.java
@@ -0,0 +1,10 @@
+package main.java;
+
+public class Main {
+    public static void main(String[] args) {
+        KeyWords keyWords = new KeyWords();
+        String fileSrc="/Users/macintosh/IdeaProjects/Task2.1/src/main/java/Pen.java";
+        String fileDst="/Users/macintosh/IdeaProjects/Task4.1/src/main/java/keywords.txt";
+        keyWords.getWords(fileSrc, fileDst);
+    }
+}
